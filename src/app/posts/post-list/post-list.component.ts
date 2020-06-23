f3c23105210c75fa73797887aa7b27dbb7bac466
Premal <premal_007@ymail.com>
2020-06-23 12:32:46 +0530

    Updated as on 23 June

--- src/app/posts/post-list/post-list.component.ts
+++ src/app/posts/post-list/post-list.component.ts
@@ -1,15 +1,17 @@
-import { Component, OnInit } from '@angular/core'
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { Post } from '../post.model';
-import { PostService } from  '../posts.service'
+import { PostService } from  '../posts.service';
+import { Subscription } from 'rxjs';
 @Component({
   selector: 'app-post-list',
   templateUrl: './post-list.component.html',
   styleUrls: ['./post-list.component.css'],
 })
 
-export class postListComponent implements OnInit{
+export class postListComponent implements OnInit, OnDestroy{
 
   posts: Post[] = [];
+  private postsSub: Subscription;
 
   constructor(public postsService: PostService){
 
@@ -17,6 +19,14 @@ export class postListComponent implements OnInit{
 
   ngOnInit(){
     this.posts = this.postsService.getPosts();
+    this.postsSub = this.postsService.getPostsUpdateListener()
+    .subscribe((posts: Post[])=> {
+      this.posts = posts;
+
+    });
+  }
+  ngOnDestroy(){
+    this.postsSub.unsubscribe();
   }
 
 }
